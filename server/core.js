8b31e76b22c6d16dc0c72354ab3dc0c3455104f4
toniku88 <133837558+toniku88@users.noreply.github.com>
2023-05-17 11:42:47 +0700

    Update core.js

--- server/core.js
+++ server/core.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import serverless from 'serverless-http';
+import compression from 'compression';
+import expressStaticGzip from 'express-static-gzip';
 import fs from "fs";
 import unirest from "unirest";
 import nodeGzip from "node-gzip";
@@ -7,8 +9,11 @@ const { gzip, ungzip} = nodeGzip;
 const app = express();
 
 app.use( async (req,res,next)=>{
+  console.log(req)
   res.end(req.url);
 });
+
+app.use(compression());
 
 export default app;
 export const handler = serverless(app);

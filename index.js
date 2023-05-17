2ede7f5a5b7d5c6b6db23ea6bcb96ba1323edc68
toniku88 <133837558+toniku88@users.noreply.github.com>
2023-05-17 12:04:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-import server from "./server/content.js";
+import home from "./server/home.js";
+import content from "./server/content.js";
 // server.listen(80, () => console.log('app listening on port 80!'));
